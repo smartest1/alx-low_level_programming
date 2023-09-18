3da7aa24895a265a0712e8b74c43a41b7b3f5aee
elias <emmysmat1@gmail.com>
2023-09-18 16:40:27 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - function to print string
+ *@str: variable containing string
+ *
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[i]);
+		count++
+	}
+}
