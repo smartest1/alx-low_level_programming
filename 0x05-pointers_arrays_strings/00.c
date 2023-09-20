1e201d0c8d632c7eba943c229a7e87be6ec16b9d
elias <emmysmat1@gmail.com>
2023-09-19 17:26:36 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/00.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Solve me
+ * 
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i= 0:
+int j;
+while (src[i] != '\0')
+i++;
+for (j=0; j <= i +1; j++)
+{
+*dest = *dest + src[j];
+}
+}
