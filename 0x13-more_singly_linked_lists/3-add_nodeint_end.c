2e1e31e45f0be092d3d654c0418816c809ea03b2
elias <emmysmat1@gmail.com>
2023-10-25 16:39:16 -0700

    fixed the function name

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * add_nodeint - function to add node
+ * add_nodeint_end - function to add node
  * @head: head node
  * @n: input data
  *
  * Return: Always 0.
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *temp;
